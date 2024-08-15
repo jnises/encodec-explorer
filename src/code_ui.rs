ba4f3e0264d9044cd8fcc4896f78c06a79a123a3
Joel Nises <joel.nises@gmail.com>
2024-08-15 15:29:58 +0200

    vertical

--- src/code_ui.rs
+++ src/code_ui.rs
@@ -4,15 +4,17 @@ pub fn draw(ui: &mut egui::Ui, codes: &mut Vec<u32>) {
     if codes.is_empty() {
         codes.push(0);
     }
-    ui.horizontal(|ui| {
+    ui.group(|ui| {
+        egui::ScrollArea::vertical().max_height(500.0).show(ui, |ui| {
+//    ui.horizontal(|ui| {
         for c in codes.iter_mut() {
             const MAX_CODE: usize = 1023;
             ui.add(DragValue::new(c).range(0..=MAX_CODE));
         }
-        const MAX_BOOKS: usize = 255;
-        ui.vertical(|ui| {
+        const MAX_LAYERS: usize = 32;
+        ui.horizontal(|ui| {
             if ui
-                .add_enabled(codes.len() <= MAX_BOOKS, egui::Button::new("+").small())
+                .add_enabled(codes.len() < MAX_LAYERS, egui::Button::new("+").small())
                 .clicked()
             {
                 codes.push(0);
@@ -23,6 +25,7 @@ pub fn draw(ui: &mut egui::Ui, codes: &mut Vec<u32>) {
             {
                 codes.pop();
             }
+        });
         });
     });
 }
