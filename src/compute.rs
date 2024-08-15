4f1a9254121930e4c54777e86907e91ccc836fb2
Joel Nises <joel.nises@gmail.com>
2024-08-15 21:31:14 +0200

    handle dc offset

--- src/compute.rs
+++ src/compute.rs
@@ -29,16 +29,29 @@ impl Compute {
             }
         }
         let code_tensor = Tensor::from_vec(v, (1, codes.len(), SEQUENCE_LEN), &self.device)?;
-        let samples = self.model.decode(&code_tensor)?.i(0)?.i(0)?;
+        let all_samples = self.model.decode(&code_tensor)?.i(0)?.i(0)?;
         const FRAGMENT_SIZE: usize = 320;
-        Ok((0..FRAGMENT_SIZE)
-            .map(|i| {
-                let w = i as f32 / (FRAGMENT_SIZE as f32 - 1.0);
-                Ok::<_, candle_core::Error>(
-                    w * samples.i(FRAGMENT_SIZE + i)?.to_scalar::<f32>()?
-                        + (1.0 - w) * samples.i(2 * FRAGMENT_SIZE + i)?.to_scalar::<f32>()?,
-                )
-            })
-            .collect::<Result<Vec<_>, _>>()?)
+        let weights = Tensor::from_vec(
+            (0..FRAGMENT_SIZE)
+                .map(|i| i as f32 / (FRAGMENT_SIZE as f32 - 1.0))
+                .collect(),
+            (FRAGMENT_SIZE,),
+            &self.device,
+        )?;
+        let samples = ((all_samples.i(FRAGMENT_SIZE..(2 * FRAGMENT_SIZE))? * &weights)?
+            + (all_samples.i((2 * FRAGMENT_SIZE)..(3 * FRAGMENT_SIZE))? * (1.0 - weights))?)?;
+        let mean = samples.mean_all()?;
+        //mean.to_scalar::<f32>().unwrap();
+        let dc0 = (samples.broadcast_sub(&mean))?;
+        Ok(dc0.to_vec1()?)
+        // let samples = (0..FRAGMENT_SIZE)
+        //     .map(|i| {
+        //         let w = i as f32 / (FRAGMENT_SIZE as f32 - 1.0);
+        //         Ok::<_, candle_core::Error>(
+        //             w * all_samples.i(FRAGMENT_SIZE + i)?.to_scalar::<f32>()?
+        //                 + (1.0 - w) * all_samples.i(2 * FRAGMENT_SIZE + i)?.to_scalar::<f32>()?,
+        //         )
+        //     })
+        //     .collect::<Result<Vec<_>, _>>()?;
     }
 }
