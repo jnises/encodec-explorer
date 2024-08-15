45768683ef2547b1ff8a554f63dc8b315f039363
Joel Nises <joel.nises@gmail.com>
2024-08-15 07:14:23 +0200

    fix

--- src/app.rs
+++ src/app.rs
@@ -1,3 +1,5 @@
+use egui::{emath, epaint, pos2, vec2, Color32, Pos2, Rect, Stroke};
+
 use crate::worker;
 
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
@@ -62,30 +64,27 @@ impl eframe::App for EncodecExplorer {
 
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.add(egui::Slider::new(&mut self.code, 0..=1023).text("code"));
+            draw_buffer(ui, self.worker.as_ref().unwrap().samples());
         });
 
         self.worker.as_mut().unwrap().set_code(self.code).unwrap();
     }
 }
 
-fn draw_buffer() {
-
-    // let plot_width = ui.available_width().min(300.);
-    // let (_, rect) = ui.allocate_space(vec2(plot_width, plot_width * 0.5));
-    // let p = ui.painter_at(rect);
-    // p.rect_filled(rect, 10f32, Color32::BLACK);
-    // let to_rect = emath::RectTransform::from_to(
-    //     Rect::from_x_y_ranges(0.0..=(VIS_SIZE / 2) as f32, -1.0..=1.0),
-    //     rect,
-    // );
-    // let line: Vec<Pos2> = left_vis_buffer
-    //     .iter()
-    //     .copied()
-    //     .skip(i)
-    //     .take(VIS_SIZE / 2)
-    //     .enumerate()
-    //     .map(|(x, y)| to_rect * pos2(x as f32, y))
-    //     .collect();
-    // debug_assert_eq!(line.len(), VIS_SIZE / 2);
-    // p.add(epaint::Shape::line(line, Stroke::new(1f32, Color32::GRAY)));
+fn draw_buffer(ui: &mut egui::Ui, buffer: &[f32]) {
+    let plot_width = ui.available_width().min(300.);
+    let (_, rect) = ui.allocate_space(vec2(plot_width, plot_width * 0.5));
+    let p = ui.painter_at(rect);
+    p.rect_filled(rect, 10f32, Color32::BLACK);
+    let to_rect = emath::RectTransform::from_to(
+        Rect::from_x_y_ranges(0.0..=(buffer.len() - 1) as f32, -1.0..=1.0),
+        rect,
+    );
+    let line: Vec<Pos2> = buffer
+        .iter()
+        .copied()
+        .enumerate()
+        .map(|(x, y)| to_rect * pos2(x as f32, y))
+        .collect();
+    p.add(epaint::Shape::line(line, Stroke::new(1f32, Color32::GRAY)));
 }
