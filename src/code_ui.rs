c8ede79ed44d13cb40ecfce0ca1cdd7dce456fa2
Joel Nises <joel.nises@gmail.com>
2024-08-15 15:37:57 +0200

    truncate

--- src/code_ui.rs
+++ src/code_ui.rs
@@ -5,27 +5,31 @@ pub fn draw(ui: &mut egui::Ui, codes: &mut Vec<u32>) {
         codes.push(0);
     }
     ui.group(|ui| {
-        egui::ScrollArea::vertical().max_height(500.0).show(ui, |ui| {
-//    ui.horizontal(|ui| {
-        for c in codes.iter_mut() {
-            const MAX_CODE: usize = 1023;
-            ui.add(DragValue::new(c).range(0..=MAX_CODE));
-        }
-        const MAX_LAYERS: usize = 32;
-        ui.horizontal(|ui| {
-            if ui
-                .add_enabled(codes.len() < MAX_LAYERS, egui::Button::new("+").small())
-                .clicked()
-            {
-                codes.push(0);
-            }
-            if ui
-                .add_enabled(codes.len() > 1, egui::Button::new("-").small())
-                .clicked()
-            {
-                codes.pop();
-            }
-        });
-        });
+        egui::ScrollArea::vertical()
+            .max_height(500.0)
+            .show(ui, |ui| {
+                for (i, c) in codes.iter_mut().enumerate() {
+                    const MAX_CODE: usize = 1023;
+                    if ui.add(DragValue::new(c).range(0..=MAX_CODE)).changed() {
+                        codes.truncate(i + 1);
+                        break;
+                    }
+                }
+                const MAX_LAYERS: usize = 32;
+                ui.horizontal(|ui| {
+                    if ui
+                        .add_enabled(codes.len() < MAX_LAYERS, egui::Button::new("+").small())
+                        .clicked()
+                    {
+                        codes.push(0);
+                    }
+                    if ui
+                        .add_enabled(codes.len() > 1, egui::Button::new("-").small())
+                        .clicked()
+                    {
+                        codes.pop();
+                    }
+                });
+            });
     });
 }
