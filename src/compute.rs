1c5cf4269ee659bb68c7988164e0d70b27154893
Joel Nises <joel.nises@gmail.com>
2024-08-16 08:26:47 +0200

    cleanup

--- src/compute.rs
+++ src/compute.rs
@@ -41,17 +41,7 @@ impl Compute {
         let samples = ((all_samples.i(FRAGMENT_SIZE..(2 * FRAGMENT_SIZE))? * &weights)?
             + (all_samples.i((2 * FRAGMENT_SIZE)..(3 * FRAGMENT_SIZE))? * (1.0 - weights))?)?;
         let mean = samples.mean_all()?;
-        //mean.to_scalar::<f32>().unwrap();
         let dc0 = (samples.broadcast_sub(&mean))?;
         Ok(dc0.to_vec1()?)
-        // let samples = (0..FRAGMENT_SIZE)
-        //     .map(|i| {
-        //         let w = i as f32 / (FRAGMENT_SIZE as f32 - 1.0);
-        //         Ok::<_, candle_core::Error>(
-        //             w * all_samples.i(FRAGMENT_SIZE + i)?.to_scalar::<f32>()?
-        //                 + (1.0 - w) * all_samples.i(2 * FRAGMENT_SIZE + i)?.to_scalar::<f32>()?,
-        //         )
-        //     })
-        //     .collect::<Result<Vec<_>, _>>()?;
     }
 }
