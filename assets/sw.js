7d57e0205ac4ce58dc716f55949ffe3418f537ec
Joel Nises <joel.nises@gmail.com>
2024-08-18 20:37:11 +0200

    cleanup

--- assets/sw.js
+++ assets/sw.js
@@ -1,4 +1,4 @@
-var cacheName = 'egui-template-pwa';
+var cacheName = 'encodec-explorer-pwa';
 var filesToCache = [
   './',
   './index.html',
--- get-model.rs
+++ get-model.rs
@@ -2,14 +2,7 @@
 //! ```cargo
 //! [dependencies]
 //! hf-hub = "0.3.2"
-//! #safetensors = "0.4"
-//! #memmap2 = "0.9"
 //! ```
-
-// use memmap2::MmapOptions;
-// use safetensors::SafeTensors;
-// use std::fs::File;
-// use std::path::Path;
 
 fn main() {
     let model_path = hf_hub::api::sync::Api::new()
@@ -23,26 +16,4 @@ fn main() {
         std::path::Path::new(&std::env::var("TRUNK_STAGING_DIR").unwrap()).join("model.safetensors"),
     )
     .unwrap();
-    // let file = File::open(&model_path).unwrap();
-    // let buffer = unsafe { MmapOptions::new().map(&file).unwrap() };
-    // let tensors = SafeTensors::deserialize(&buffer).unwrap();
-    // let filter_fn = |n: &str| !n.starts_with("encoder");
-    // let filtered: Vec<_> = tensors
-    //     .names()
-    //     .into_iter()
-    //     .filter(|n| filter_fn(n.as_str()))
-    //     .collect();
-    // let mut meta = SafeTensors::read_metadata(&buffer)
-    //     .unwrap()
-    //     .1
-    //     .metadata()
-    //     .clone()
-    //     .unwrap();
-    // meta.retain(|k, _| filter_fn(k.as_str()));
-    // safetensors::serialize_to_file(
-    //     filtered.iter().map(|n| (n, tensors.tensor(n).unwrap())),
-    //     &Some(meta),
-    //     &Path::new("decoder_only.safetensors"),
-    // )
-    // .unwrap();
 }
