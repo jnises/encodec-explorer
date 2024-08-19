5853fa71e86e7f6ca425054ebe9998387a67e8c4
Joel Nises <joel.nises@gmail.com>
2024-08-19 21:09:16 +0200

    use sliders (#2)
    * use sliders
    
    * update readme
    
    * fix lints

--- src/code_ui.rs
+++ src/code_ui.rs
@@ -1,4 +1,4 @@
-use egui::DragValue;
+use egui::Slider;
 
 pub fn draw(ui: &mut egui::Ui, codes: &mut Vec<u32>) {
     if codes.is_empty() {
@@ -8,26 +8,23 @@ pub fn draw(ui: &mut egui::Ui, codes: &mut Vec<u32>) {
         egui::ScrollArea::vertical()
             .max_height(500.0)
             .show(ui, |ui| {
-                for (i, c) in codes.iter_mut().enumerate() {
-                    const MAX_CODE: usize = 1023;
-                    if ui.add(DragValue::new(c).range(0..=MAX_CODE)).changed() {
-                        codes.truncate(i + 1);
-                        break;
-                    }
+                for c in codes.iter_mut() {
+                    const MAX_CODE: u32 = 1023;
+                    ui.add(Slider::new(c, 0..=MAX_CODE));
                 }
                 const MAX_LAYERS: usize = 32;
                 ui.horizontal(|ui| {
-                    if ui
-                        .add_enabled(codes.len() < MAX_LAYERS, egui::Button::new("+").small())
-                        .clicked()
-                    {
-                        codes.push(0);
-                    }
                     if ui
                         .add_enabled(codes.len() > 1, egui::Button::new("-").small())
                         .clicked()
                     {
                         codes.pop();
+                    }
+                    if ui
+                        .add_enabled(codes.len() < MAX_LAYERS, egui::Button::new("+").small())
+                        .clicked()
+                    {
+                        codes.push(0);
                     }
                 });
             });
