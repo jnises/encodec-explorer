6d06e96dd3483471fef11d1a952cf29ab7119a17
jnises <joel.nises@gmail.com>
2024-09-29 19:36:28 +0000

    Deploying to gh-pages from @ jnises/encodec-explorer@f80e6f97eb018a6170330f4d19a038f308ef3d43 🚀

---
+++ sw.js
@@ -0,0 +1,25 @@
+var cacheName = 'encodec-explorer-pwa';
+var filesToCache = [
+  './',
+  './index.html',
+  './encodec-explorer.js',
+  './encodec-explorer_bg.wasm',
+];
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (response) {
+      return response || fetch(e.request);
+    })
+  );
+});
